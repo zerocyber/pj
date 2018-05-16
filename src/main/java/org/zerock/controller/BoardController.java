1a4a6c58a74fd3b3bbc34630c9d0a21682741adc
whwp0913 <whwp0913@whwp0913ui-MacBook-Air.local>
2018-05-16 11:42:18 +0900

    root-context modi & BoardController mody 2

--- src/main/java/org/zerock/controller/BoardController.java
+++ src/main/java/org/zerock/controller/BoardController.java
@@ -1,5 +1,11 @@
 package org.zerock.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/board/*")
+@Controller
 public class BoardController {
 
+	
 }
