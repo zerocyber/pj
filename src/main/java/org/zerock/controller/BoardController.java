4e685acade050a82a658d38a42368d251cfee6be
whwp0913 <whwp0913@whwp0913ui-MacBook-Air.local>
2018-05-16 18:41:28 +0900

    add utils uriComponent 2

--- src/main/java/org/zerock/controller/BoardController.java
+++ src/main/java/org/zerock/controller/BoardController.java
@@ -2,6 +2,7 @@ package org.zerock.controller;
 
 import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.core.userdetails.ReactiveUserDetailsServiceResourceFactoryBean;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
