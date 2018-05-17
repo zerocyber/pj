58dc680a2bd4a88232f662a975beb2902944ad44
whwp0913 <whwp0913@whwp0913ui-MacBook-Air.local>
2018-05-17 12:26:49 +0900

    Reply Mapper Test Clear 3

--- src/main/java/org/zerock/mapper/ReplyMapper.java
+++ src/main/java/org/zerock/mapper/ReplyMapper.java
@@ -1,6 +1,7 @@
 package org.zerock.mapper;
 
 import java.util.List;
+import java.util.Map;
 
 import org.zerock.domain.Criteria;
 import org.zerock.domain.ReplyVO;
@@ -9,12 +10,14 @@ public interface ReplyMapper {
 	
 	public void inset(ReplyVO vo);
 	
-	public List<ReplyVO> list(int bno, Criteria cri);
+	public List<ReplyVO> list(Map<String,Object> map);
 	
 	public void update(ReplyVO vo);
 	
 	public void delete(int rno);
 	
-	public int count(int bno, Criteria cri);
+	public int count(int bno);
+	
+	public ReplyVO read(int rno);
 
 }
