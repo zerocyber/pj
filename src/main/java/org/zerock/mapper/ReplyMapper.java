e80b5e207fd4c6b356fc5f80bfe0e35dacdf5bbf
whwp0913 <whwp0913@whwp0913ui-MacBook-Air.local>
2018-05-17 12:40:55 +0900

    Reply Service add 3

--- src/main/java/org/zerock/mapper/ReplyMapper.java
+++ src/main/java/org/zerock/mapper/ReplyMapper.java
@@ -8,7 +8,7 @@ import org.zerock.domain.ReplyVO;
 
 public interface ReplyMapper {
 	
-	public void inset(ReplyVO vo);
+	public void insert(ReplyVO vo);
 	
 	public List<ReplyVO> list(Map<String,Object> map);
 	
