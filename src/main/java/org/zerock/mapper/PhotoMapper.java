ab0b77eb78f3ad740dd2fd5834d522de498b4949
ask9751 <jonghun3433@gmail.com>
2018-06-01 15:09:08 +0900

    Photo....

---
+++ src/main/java/org/zerock/mapper/PhotoMapper.java
@@ -0,0 +1,37 @@
+package org.zerock.mapper;
+
+import java.util.List;
+
+import org.zerock.domain.PhotoVO;
+import org.zerock.domain.Criteria;
+
+public interface PhotoMapper {
+
+	public List<PhotoVO> list(Criteria cri);
+	
+	public PhotoVO read(int pno);
+	
+	public int insert(PhotoVO vo);
+	
+	public int update(PhotoVO vo);
+	
+	public int delete(int pno);
+	
+	public int count(Criteria cri);
+	
+	public List<PhotoVO> searchList(Criteria cri);
+
+	public int addViews(int pno);
+	
+	//  업로드 
+	public int addImage(PhotoVO vo);
+	// 파일 삭제
+	public int removeImage(int pno);
+	// 파일 조회 
+	public String[] searchImage(int pno);
+	// 글 수정 시 파일 삭제
+	public int removeImages(String[] deleteImages);
+	// 글 수정 시 파일 등록
+	public int modiImage(PhotoVO vo);
+	
+}
